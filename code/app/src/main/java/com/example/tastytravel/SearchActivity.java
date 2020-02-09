45aadb8ca319cb1c33c8148264260fe8f62c5bef
savagem7 <i0g1n4oj>
2020-02-09 00:18:22 +0000

    change about page and search page started.

--- code/app/src/main/java/com/example/tastytravel/SearchActivity.java
+++ code/app/src/main/java/com/example/tastytravel/SearchActivity.java
@@ -2,13 +2,41 @@ package com.example.tastytravel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SearchActivity extends AppCompatActivity {
+
+    Button searchBtn;
+    Button closeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        searchBtn = findViewById(R.id.searchBtn);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), MapActivity.class);
+                // pass info to 2nd screen
+                startActivity(startIntent);
+            }
+        });
+
+        closeBtn = findViewById(R.id.closeBtn);
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
+                // pass info to 2nd screen
+                startActivity(startIntent);
+            }
+        });
     }
 }
