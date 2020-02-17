ed03a07c3042adc6fc4f293e0d90b93524789917
Michael Savage <michael.savage7@mail.dcu.ie>
2020-02-17 19:09:54 +0000

    develop enablelocation function

--- code/app/src/main/java/com/example/tastytravel/SearchActivity.java
+++ code/app/src/main/java/com/example/tastytravel/SearchActivity.java
@@ -1,38 +1,31 @@
 package com.example.tastytravel;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.content.DialogInterface;
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.mapbox.geojson.Point;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.location.LocationComponent;
 
-import com.mapbox.api.geocoding.v5.GeocodingCriteria;
-import com.mapbox.api.geocoding.v5.MapboxGeocoding;
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
 
-    private static final int MY_PERMISSIONS_REQUEST_READ_COARSE_LOCATION = 1;
-    TextView searchText, closeText, result;
-    Button fetchLocation;
-    EditText user1Location, user2Location;
+    public static final String LocationOne = "searchActivity.locationOne";
+    public static final String LocationTwo = "searchActivity.locationTwo";
 
-    private FusedLocationProviderClient fusedLocationClient;
+    TextView searchText, closeText;
+    EditText locationText, locationText2;
+    Button locationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,24 +33,15 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         searchText = findViewById(R.id.searchText);
-        closeText = findViewById(R.id.closeText);
-        fetchLocation = findViewById(R.id.locationBtn);
-        user1Location = findViewById(R.id.locationText);
-        user2Location = findViewById(R.id.locationText2);
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        result = findViewById(R.id.result);
-
-
         searchText.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent showMap = new Intent(getApplicationContext(), MapActivity.class);
-                startActivity(showMap);
+                openMap();
             }
         });
 
+        closeText = findViewById(R.id.closeText);
         closeText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -65,108 +49,31 @@ public class SearchActivity extends AppCompatActivity {
                 startActivity(startIntent);
             }
         });
-
-        fetchLocation.setOnClickListener(new View.OnClickListener() {
+        locationBtn = findViewById(R.id.locationBtn);
+        locationBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                fetchLocation();
+                enableLocation();
             }
         });
     }
 
-    private void fetchLocation() {
-        // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(SearchActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+    public void openMap() {
+        //DCU is -6.2564 , 53.3861
+        locationText = findViewById(R.id.locationText);
+        String locationOne = locationText.getText().toString();
 
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(SearchActivity.this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
+        // o'connell street is -6.2607 , 53.3508
+        locationText2 = findViewById(R.id.locationText2);
+        String locationTwo = locationText2.getText().toString();
 
-                new AlertDialog.Builder(this).setTitle("Location is Required")
-                        .setMessage("Your location is required for more precise search results")
-
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                ActivityCompat.requestPermissions(SearchActivity.this,
-                                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                                    MY_PERMISSIONS_REQUEST_READ_COARSE_LOCATION);
-
-                                user1Location.setText("Using Current Location");
-                            }
-                        })
-
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        })
-                        .create().show();
-
-            } else {
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(SearchActivity.this,
-                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                    MY_PERMISSIONS_REQUEST_READ_COARSE_LOCATION);
-
-                // MY_PERMISSIONS_REQUEST_READ_COARSE_LOCATION is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        } else {
-            // Permission has already been granted
-            fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    // Got last known location. In some rare situations this can be null.
-                    if (location != null) {
-                        // Logic to handle location object
-                        Double latitude = location.getLatitude();
-                        Double longitude = location.getLongitude();
-
-                        setText(latitude, longitude);
-                    }
-                }
-            });
-        }
+        Intent showMap = new Intent(getApplicationContext(), MapActivity.class);
+        showMap.putExtra(LocationOne, locationOne);
+        showMap.putExtra(LocationTwo, locationTwo);
+        startActivity(showMap);
     }
 
-    public void setText(Double latitude, Double longitude) {
-        String address = getAddress(latitude, longitude);
-
-        user1Location.setText("Using Current Location");
-        result.setText(address);
-    }
-
-    // Reverse Geocoding
-    public String getAddress(Double latitude, Double longitude){
-        MapboxGeocoding reverseGeocode = MapboxGeocoding.builder()
-                .accessToken(getString(R.string.mapbox_access_token))
-                .query(Point.fromLngLat(longitude, latitude))
-                .geocodingTypes(GeocodingCriteria.TYPE_ADDRESS)
-                .build();
-
-        // The result of this reverse geocode will give you "Pennsylvania Ave NW"
-        return reverseGeocode.toString();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if(requestCode==MY_PERMISSIONS_REQUEST_READ_COARSE_LOCATION){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-
-            } else {
-
-            }
-        }
+    public void enableLocation(){
 
     }
 }
