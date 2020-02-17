4b3db17b2f7fdc5b3a34dcb8cd108147cdbd5767
Michael Savage <michael.savage7@mail.dcu.ie>
2020-02-17 22:26:10 +0000

    Update SearchActivity.java

--- code/app/src/main/java/com/example/tastytravel/SearchActivity.java
+++ code/app/src/main/java/com/example/tastytravel/SearchActivity.java
@@ -2,21 +2,13 @@ package com.example.tastytravel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.mapbox.android.core.permissions.PermissionsManager;
-import com.mapbox.mapboxsdk.location.LocationComponent;
 
-import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -25,12 +17,14 @@ public class SearchActivity extends AppCompatActivity {
 
     TextView searchText, closeText;
     EditText locationText, locationText2;
-    Button locationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        locationText = findViewById(R.id.locationText);
+        locationText2 = findViewById(R.id.locationText2);
 
         searchText = findViewById(R.id.searchText);
         searchText.setOnClickListener(new View.OnClickListener() {
@@ -40,7 +34,6 @@ public class SearchActivity extends AppCompatActivity {
                 openMap();
             }
         });
-
         closeText = findViewById(R.id.closeText);
         closeText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,31 +42,18 @@ public class SearchActivity extends AppCompatActivity {
                 startActivity(startIntent);
             }
         });
-        locationBtn = findViewById(R.id.locationBtn);
-        locationBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                enableLocation();
-            }
-        });
     }
 
     public void openMap() {
         //DCU is -6.2564 , 53.3861
-        locationText = findViewById(R.id.locationText);
         String locationOne = locationText.getText().toString();
 
         // o'connell street is -6.2607 , 53.3508
-        locationText2 = findViewById(R.id.locationText2);
         String locationTwo = locationText2.getText().toString();
 
         Intent showMap = new Intent(getApplicationContext(), MapActivity.class);
-        showMap.putExtra(LocationOne, locationOne);
-        showMap.putExtra(LocationTwo, locationTwo);
+        showMap.putExtra(LocationOne, "-6.2564 , 53.3861");
+        showMap.putExtra(LocationTwo, "-6.2607 , 53.3508");
         startActivity(showMap);
-    }
-
-    public void enableLocation(){
-
     }
 }
