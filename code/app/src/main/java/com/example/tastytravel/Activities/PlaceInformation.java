88ead13b9637cdfbe447453c24a1ce9af78ba69f
Gerard Slowey <gerard.slowey2@mail.dcu.ie>
2020-03-06 11:57:48 +0000

    Revert "Merge branch 'master' into 'working'"
    This reverts commit df9241b31f820bec731dac5d2a844310f5f672c2

---
+++ code/app/src/main/java/com/example/tastytravel/Activities/PlaceInformation.java
@@ -0,0 +1,16 @@
+package com.example.tastytravel.Activities;
+
+public class PlaceInformation {
+
+    public String placeName;
+    public String coordinates;
+
+    public PlaceInformation() {
+
+    }
+
+    public PlaceInformation(String placeName, String coordinates) {
+        this.placeName = placeName;
+        this.coordinates = coordinates;
+    }
+}
